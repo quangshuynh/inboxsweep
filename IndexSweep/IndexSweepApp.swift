79a1621643808f9893df83876f57d0b10f135a4b
Quang <20378quang@gmail.com>
2026-09-10 11:31:13 -0400

    Initial Commit

---
+++ IndexSweep/IndexSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndexSweepApp.swift
+//  IndexSweep
+//
+//  Created by q on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct IndexSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IndexSweepTests/IndexSweepTests.swift
@@ -0,0 +1,19 @@
+//
+//  IndexSweepTests.swift
+//  IndexSweepTests
+//
+//  Created by q on 9/10/26.
+//
+
+import Testing
+@testable import IndexSweep
+
+struct IndexSweepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
